98c4f02c32042c7cec6adca5dc9f14f03b0819e7
Glenn-Richard <bglennrichard.pro@yahoo.com>
2022-03-07 16:03:52 +0100

    correction buttom code

--- app/src/main/java/com/example/go4lunch/AuthActivity.java
+++ app/src/main/java/com/example/go4lunch/AuthActivity.java
@@ -2,19 +2,17 @@ package com.example.go4lunch;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.go4lunch.databinding.ActivityAuthMethodPickerBinding;
-import com.example.go4lunch.databinding.ActivityRootBindingBinding;
+import com.example.go4lunch.databinding.ActivitySplashScreenBinding;
 import com.firebase.ui.auth.AuthMethodPickerLayout;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
@@ -22,41 +20,57 @@ import com.firebase.ui.auth.FirebaseUiException;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
+
 public class AuthActivity extends AppCompatActivity {
 
-    private final int AUTH_CODE = 123;
-    private ActivityAuthMethodPickerBinding binding;
+    private ActivitySplashScreenBinding binding;
+    FirebaseAuth mAuth;
+    private final ActivityResultLauncher<Intent> activityLauncher =
+            (ActivityResultLauncher<Intent>) registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(), this::onActivityResult);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityAuthMethodPickerBinding.inflate(getLayoutInflater());
+        binding = ActivitySplashScreenBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        mAuth = FirebaseAuth.getInstance();
+        startLoginActivity();
+
     }
 
-    private void startSignInActivity(){
+    private void requireAuthentication() {
+        if (mAuth.getCurrentUser() != null) {
+            startMainActivity();
+        } else {
+            startLoginActivity();
+        }
+    }
 
-        // Choose authentication providers
-        List<AuthUI.IdpConfig> providers =
-                Collections.singletonList(new AuthUI.IdpConfig.EmailBuilder().build());
-
-        // Launch the activity
-        startActivityForResult(
+    private void startLoginActivity() {
+        Log.i("LOGING", "____ON_REQUEST_LOGIN______");
+        activityLauncher.launch(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
-                        .setTheme(R.style.LoginTheme)
-                        .setAvailableProviders(providers)
-                        .setIsSmartLockEnabled(false, true)
-                        .setLogo(R.drawable.ic_logout_24)
-                        .build(),
-                AUTH_CODE);
+                        .setAvailableProviders(getAuthProviders())
+                        .setLogo(R.drawable.logo)
+                        .setIsSmartLockEnabled(false)
+                        .setAuthMethodPickerLayout(getCustomAuthLayout())
+                        .setTheme(R.style.NoActionBarSysUITransparentTheme)
+                        .build());
     }
+
+    private List<AuthUI.IdpConfig> getAuthProviders() {
+        return Arrays.asList(
+                new AuthUI.IdpConfig.GoogleBuilder()
+                        .setSignInOptions(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build());
+    }
+
     private AuthMethodPickerLayout getCustomAuthLayout() {
         return new AuthMethodPickerLayout.Builder(R.layout.activity_auth_method_picker)
                 .setGoogleButtonId(R.id.google_login_btn)
@@ -64,31 +78,40 @@ public class AuthActivity extends AppCompatActivity {
                 .build();
     }
 
-
-    private void startMainActivity() {
-        final Intent startMainActivityIntent = new Intent(this, AuthActivity.class);
-        startActivity(startMainActivityIntent);
-        finish();
+    private void onActivityResult(ActivityResult result) {
+        Log.i("LOGING", "____ON_RESULT_LOGIN______");
+        if (result.getResultCode() == Activity.RESULT_OK) {
+            startMainActivity();
+        } else {
+            binding.getRoot().setVisibility(View.VISIBLE);
+            if (result.getResultCode() == Activity.RESULT_CANCELED) {
+                showLoginError(R.string.login_canceled);
+            } else handleLoginFailure(result.getData());
+        }
     }
 
-    private void connectionStatement(){
-
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            // Name, email address, and profile photo Url
-            String name = user.getDisplayName();
-            String email = user.getEmail();
-            Uri photoUrl = user.getPhotoUrl();
-
-            // Check if user's email is verified
-            boolean emailVerified = user.isEmailVerified();
-
-            // The user's ID, unique to the Firebase project. Do NOT use this value to
-            // authenticate with your backend server, if you have one. Use
-            // FirebaseUser.getIdToken() instead.
-            String uid = user.getUid();
+    @SuppressWarnings({"ConstantConditions"})
+    private void handleLoginFailure(Intent loginResponseData) {
+        try {
+            final FirebaseUiException responseError =
+                    IdpResponse.fromResultIntent(loginResponseData).getError();
+            if (responseError.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                showLoginError(R.string.login_failed_network_error);
+            } else {
+                showLoginError(R.string.login_failed_unknown_error);
+            }
+        } catch (NullPointerException e) {
+            showLoginError(R.string.login_failed_unknown_error);
         }
-        else{
-        }
+    }
+
+    private void showLoginError(int resourceId) {
+       Toast.makeText(this,getString(resourceId),Toast.LENGTH_LONG).show();
+    }
+
+    private void startMainActivity() {
+        final Intent startMainActivityIntent = new Intent(this, MainActivity.class);
+        startActivity(startMainActivityIntent);
+        finish();
     }
 }
